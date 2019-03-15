2cf8a6f0f5a2446951c143b6fd200d7d59356da1
Bkhufa <47885514+Bkhufa@users.noreply.github.com>
2019-03-15 10:14:09 +0700

    Add files via upload

---
+++ soal4/soal4.c
@@ -0,0 +1,67 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <unistd.h>
+#include <syslog.h>
+#include <string.h>
+#include <time.h>
+
+int main() {
+  pid_t pid, sid;
+
+  pid = fork();
+
+  if (pid < 0) {
+    exit(EXIT_FAILURE);
+  }
+
+  if (pid > 0) {
+    exit(EXIT_SUCCESS);
+  }
+
+  umask(0);
+
+  sid = setsid();
+
+  if (sid < 0) {
+    exit(EXIT_FAILURE);
+  }
+
+  if ((chdir("/")) < 0) {
+    exit(EXIT_FAILURE);
+  }
+
+  close(STDIN_FILENO);
+  close(STDOUT_FILENO);
+  close(STDERR_FILENO);
+
+  char pathFile[] = "home/bryan/Documents/makanan/makan_enak.txt";
+  int namafile = 1;
+
+  while(1) {
+    char nama[3];
+    sprintf(nama,"%d",namafile);
+    struct stat statFile;
+    stat (pathFile, &statFile);
+    time_t timeFile2 = statFile.st_atime;
+
+    //mengecek waktu terakhir file dibuka
+    if (difftime(time(NULL), timeFile2) <= 30) {
+	//membuat file diet
+
+	char pathHasil[]="/home/bryan/Documents/makanan/makan_sehat";
+	strcat(pathHasil, nama);
+	strcat(pathHasil, ".txt");
+
+	FILE *hasil = fopen(pathHasil, "w");
+	fclose(hasil);
+	namafile++;
+	}
+
+    sleep(5);
+  }
+  exit(EXIT_SUCCESS);
+}
